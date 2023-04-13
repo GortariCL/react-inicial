e645e79bc5be99a31d13b20460a46570abe57bb5
Fernando Gortari <fgortari@mobdev.cl>
2023-04-13 17:19:30 -0400

    Sección 3 Finalizada

--- hola-mundo/src/App.js
+++ hola-mundo/src/App.js
@@ -2,14 +2,19 @@ import logo from "./logo.svg";
 import "./App.css";
 import Greeting from "./components/pure/Greeting";
 import GreetingF from "./components/pure/GreetingF";
+import TaskList from "./components/container/TaskList";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {/* Componente propio Greeting.jsx */}
         {/* <Greeting name="Fernando" /> */}
-        <GreetingF name="Fernando" />
+        {/* Componente de ejemplo funcional */}
+        {/* <GreetingF name="Fernando" /> */}
+        {/* Componente de listado de tareas */}
+        <TaskList />
       </header>
     </div>
   );
