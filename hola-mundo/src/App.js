7c5e7781b3c293da76ed723e4f69590bd294a969
Fernando Gortari <fgortari@mobdev.cl>
2023-04-13 15:53:20 -0400

    Sección 2 finalizada

--- hola-mundo/src/App.js
+++ hola-mundo/src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Greeting from "./components/pure/Greeting";
+import GreetingF from "./components/pure/GreetingF";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HOLA MUNDO!
-        </p>
+        {/* <Greeting name="Fernando" /> */}
+        <GreetingF name="Fernando" />
       </header>
     </div>
   );
